41ac81968fc1f2c5106732343917fa5a2295aeb2
Sergey Sapegin <ssapegin@alfabank.ru>
2022-06-10 12:42:47 +0300

    пока без БД

---
+++ back/resources/proba.py
@@ -0,0 +1,10 @@
+from sqlalchemy import text
+from flask import make_response, jsonify, request, abort, redirect
+from flask_restful import Resource as ResFree
+from hashlib import md5
+
+
+class ProbaGet(ResFree):
+    def get(self):
+        res = 'Hello World'
+        return make_response(jsonify(res), 200)
