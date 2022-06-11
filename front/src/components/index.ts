ad64b65cbc29ca41312fe96b07c9ddeb0fa42e83
Kirill Kazakov <gorgchap@gmail.com>
2022-06-11 20:46:44 +0300

    Update producer form according to logic

--- front/src/components/index.ts
+++ front/src/components/index.ts
@@ -4,4 +4,4 @@ export * from './header/header.component';
 export * from './input-field/input-field';
 export * from './login/login.component';
 export * from './main/main.component';
-export * from './producer/producer.component';
+export * from './producer/producer-page.component';
--- front/src/components/producer/validation-schema.ts
+++ front/src/components/producer/validation-schema.ts
@@ -1,24 +1,17 @@
 import * as Yup from 'yup';
 import { formModel } from './form-model';
 
-const { producerName, inn, region, commodityGroup, nameOnCard, cardNumber, cvv } = formModel;
-const innRegEx = /^\d{16}$/;
-const visaRegEx = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+const { producerName, inn, region, commodityGroup, site, csvInfo } = formModel;
 
 export const validationSchema = [
-  Yup.object().shape({
-    [producerName.name]: Yup.string().required(producerName.requiredErrorMsg),
-    [inn.name]: Yup.string().required(inn.requiredErrorMsg).matches(innRegEx, inn.invalidErrorMsg),
-    [region.name]: Yup.string().required(region.requiredErrorMsg),
-    [commodityGroup.name]: Yup.string().required(commodityGroup.requiredErrorMsg),
-  }),
-  Yup.object().shape({
-    [nameOnCard.name]: Yup.string().required(nameOnCard.requiredErrorMsg),
-    [cardNumber.name]: Yup.string()
-      .required(cardNumber.requiredErrorMsg)
-      .matches(visaRegEx, cardNumber.invalidErrorMsg),
-    [cvv.name]: Yup.string()
-      .required(cvv.requiredErrorMsg)
-      .test('len', cvv.invalidErrorMsg, val => typeof val === 'string' && val.length === 3)
-  })
+    Yup.object().shape({
+        [producerName.name]: Yup.string().required(producerName.requiredErrorMsg),
+        [inn.name]: Yup.string().required(inn.requiredErrorMsg).matches(/^\d{16}$/, inn.invalidErrorMsg),
+        [region.name]: Yup.string().required(region.requiredErrorMsg),
+        [commodityGroup.name]: Yup.string().required(commodityGroup.requiredErrorMsg),
+        [site.name]: Yup.string().required(site.requiredErrorMsg),
+    }),
+    Yup.object().shape({
+        [csvInfo.name]: Yup.string().required(csvInfo.requiredErrorMsg),
+    }),
 ];
